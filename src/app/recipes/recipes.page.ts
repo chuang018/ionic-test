b022d510620de4fa1b754f45cf525bcc8eb3b8cc
Calvin Huang <calvinhuang13@gmail.com>
2021-01-05 21:21:56 +0700

    added list of recipes

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG/1280px-Breitenlesau_Krug_Br%C3%A4u_Schnitzel.JPG',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Sphagetti',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Spaghetti_carbonara_-_Trishhhh.jpg/359px-Spaghetti_carbonara_-_Trishhhh.jpg',
+      ingredients: ['Pasta', 'Tomato Sauce', 'Salad']
+    }
+  ]
 
   constructor() { }
 
